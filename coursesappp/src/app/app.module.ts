a5b14a4b88a02265808e4859bb575081fe3e73f2
Andrei_Krasnou <krasnovandr@mail.ru>
2017-11-20 22:33:54 +0300

    add initial project strucutre

--- coursesappp/src/app/app.module.ts
+++ coursesappp/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
