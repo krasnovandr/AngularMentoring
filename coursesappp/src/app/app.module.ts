d1645164cb0e7956c1f776f7738b932e2f65c396
Andrei_Krasnou <krasnovandr@mail.ru>
2017-11-30 01:55:07 +0300

    test overlay from angular cdk

--- coursesappp/src/app/app.module.ts
+++ coursesappp/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { ToolboxComponent } from './toolbox/toolbox.component';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
-import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { CourseDetailComponent, RotiniPanel, SpagettiPanel, KeyboardTrackingPanel } from './course-detail/course-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CoursesService } from './courses.service';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PortalModule } from '@angular/cdk/portal';
 
 
 @NgModule({
@@ -20,12 +22,17 @@ import { CoursesService } from './courses.service';
     HeaderComponent,
     FooterComponent,
     ToolboxComponent,
+    RotiniPanel,
+    SpagettiPanel, KeyboardTrackingPanel
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    OverlayModule,
+    PortalModule
   ],
   providers: [CoursesService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [RotiniPanel, SpagettiPanel,KeyboardTrackingPanel]
 })
 export class AppModule { }
--- coursesappp/src/app/course-detail/course-detail.component.ts
+++ coursesappp/src/app/course-detail/course-detail.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component, OnInit, Input, Output, EventEmitter, ViewContainerRef, QueryList,
+  ViewChild,
+  ViewChildren, ViewEncapsulation
+} from '@angular/core';
 import { Course } from '../courses';
+import { CdkOverlayOrigin, Overlay, OverlayConfig } from '@angular/cdk/overlay';
+import { CdkPortal, ComponentPortal, Portal } from '@angular/cdk/portal';
+
+
+import { filter } from 'rxjs/operators/filter';
+import { tap } from 'rxjs/operators/tap';
 
 @Component({
   selector: 'app-course-detail',
@@ -9,8 +19,14 @@ import { Course } from '../courses';
 export class CourseDetailComponent implements OnInit {
   @Input() course: Course;
   @Output() onDelete: EventEmitter<any> = new EventEmitter<any>();
+  nextPosition = 0;
+  isMenuOpen: boolean = false;
 
-  constructor() { }
+  @ViewChildren(CdkPortal) templatePortals: QueryList<Portal<any>>;
+  @ViewChild(CdkOverlayOrigin) _overlayOrigin: CdkOverlayOrigin;
+  @ViewChild('tortelliniOrigin') tortelliniOrigin: CdkOverlayOrigin;
+  @ViewChild('tortelliniTemplate') tortelliniTemplate: CdkPortal;
+  constructor(public overlay: Overlay, public viewContainerRef: ViewContainerRef) { }
 
   ngOnInit() {
   }
@@ -20,4 +36,124 @@ export class CourseDetailComponent implements OnInit {
     this.onDelete.emit(course);
   }
 
+  openRotiniPanel() {
+    let config = new OverlayConfig();
+
+    config.positionStrategy = this.overlay.position()
+      .global()
+      .left(`${this.nextPosition}px`)
+      .top(`${this.nextPosition}px`);
+
+    this.nextPosition += 30;
+
+    let overlayRef = this.overlay.create(config);
+    overlayRef.attach(new ComponentPortal(RotiniPanel, this.viewContainerRef));
+  }
+
+  openFusilliPanel() {
+    let config = new OverlayConfig();
+
+    config.positionStrategy = this.overlay.position()
+      .global()
+      .centerHorizontally()
+      .top(`${this.nextPosition}px`);
+
+    this.nextPosition += 30;
+
+    let overlayRef = this.overlay.create(config);
+    overlayRef.attach(this.templatePortals.first);
+  }
+
+  openSpaghettiPanel() {
+    // TODO(jelbourn): separate overlay demo for connected positioning.
+    let strategy = this.overlay.position()
+      .connectedTo(
+      this._overlayOrigin.elementRef,
+      { originX: 'start', originY: 'bottom' },
+      { overlayX: 'start', overlayY: 'top' });
+
+    let config = new OverlayConfig({ positionStrategy: strategy });
+    let overlayRef = this.overlay.create(config);
+
+    overlayRef.attach(new ComponentPortal(SpagettiPanel, this.viewContainerRef));
+  }
+
+  openTortelliniPanel() {
+    let strategy = this.overlay.position()
+      .connectedTo(
+      this.tortelliniOrigin.elementRef,
+      { originX: 'start', originY: 'bottom' },
+      { overlayX: 'end', overlayY: 'top' });
+
+    let config = new OverlayConfig({ positionStrategy: strategy });
+    let overlayRef = this.overlay.create(config);
+
+    overlayRef.attach(this.tortelliniTemplate);
+  }
+
+  openPanelWithBackdrop() {
+    let config = new OverlayConfig({
+      hasBackdrop: true,
+      backdropClass: 'cdk-overlay-transparent-backdrop',
+      positionStrategy: this.overlay.position().global().centerHorizontally()
+    });
+
+    let overlayRef = this.overlay.create(config);
+    overlayRef.attach(this.templatePortals.first);
+    overlayRef.backdropClick().subscribe(() => overlayRef.detach());
+  }
+
+  openKeyboardTracking() {
+    let config = new OverlayConfig();
+
+    config.positionStrategy = this.overlay.position()
+      .global()
+      .centerHorizontally()
+      .top(`${this.nextPosition}px`);
+
+    this.nextPosition += 30;
+
+    let overlayRef = this.overlay.create(config);
+    const componentRef = overlayRef
+      .attach(new ComponentPortal(KeyboardTrackingPanel, this.viewContainerRef));
+
+    overlayRef.keydownEvents()
+      .pipe(
+      tap(e => componentRef.instance.lastKeydown = e.key),
+      filter(e => e.key === 'Escape')
+      ).subscribe(() => overlayRef.detach());
+  }
+
 }
+
+/** Simple component to load into an overlay */
+@Component({
+  moduleId: module.id,
+  selector: 'rotini-panel',
+  template: '<p class="demo-rotini">Rotini {{value}}</p>'
+})
+export class RotiniPanel {
+  value: number = 9000;
+}
+
+/** Simple component to load into an overlay */
+@Component({
+  selector: 'spagetti-panel',
+  template: '<div class="demo-spagetti">Spagetti {{value}}</div>'
+})
+export class SpagettiPanel {
+  value: string = 'Omega';
+}
+
+/** Simple component to load into an overlay */
+@Component({
+  selector: 'keyboard-panel',
+  template: '<div class="demo-keyboard">Last Keydown: {{ lastKeydown }}</div>'
+})
+export class KeyboardTrackingPanel {
+  lastKeydown = '';
+}
+
+
+
+
