bc4b6773b234fb87c6dc86409672703287fe68de
Andrei_Krasnou <krasnovandr@mail.ru>
2018-01-07 15:11:01 +0300

    add backend mock server for the task 7

--- coursesappp/src/app/shared-components/header/header.component.ts
+++ coursesappp/src/app/shared-components/header/header.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { AuthorizationService } from '../../services/authorization.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../../models/user';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+
   private userName: string;
+  private subscription: Subscription;
+
   constructor(
     private authService: AuthorizationService,
     private router: Router,
     private cd: ChangeDetectorRef) { }
 
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   ngOnInit() {
-    this.authService.userInfo.subscribe(user => {
+    this.subscription = this.authService.userInfo.subscribe(user => {
       this.userName = user ? user.login : '';
       this.cd.markForCheck();
     });
   }
+
 
   signOut() {
     this.authService.logout();
