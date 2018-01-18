c0b1c66d508fce51640290c4ad6a7fa8533d92a1
Andrei_Krasnou <Andrei_Krasnou@epam.com>
2018-01-18 18:02:05 +0300

    start working on the courses reactive form, implement custom validation for dates

--- coursesappp/src/app/pages/course/course.component.ts
+++ coursesappp/src/app/pages/course/course.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormGroup, FormControl, Validators, FormBuilder, ValidatorFn, AbstractControl } from '@angular/forms';
+import { dateFormatValidator } from '../../validators/custom-validators';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
-export class CourseComponent implements OnInit {
 
-  constructor(private location: Location) { }
+
+
+export class CourseComponent implements OnInit {
+  courseForm: FormGroup;
+
+  constructor(private location: Location, private formBuilder: FormBuilder) { }
+
 
   ngOnInit() {
+    this.courseForm = this.formBuilder.group({
+      title: ['', [Validators.maxLength(50), Validators.required]],
+      date: ['', [Validators.required, dateFormatValidator()]],
+      description: ['', [Validators.required, Validators.maxLength(500)]],
+      duration: ['', [Validators.required]],
+    });
   }
 
 
-  onSave(title: string, date: Date, description: string, duration: number) {
-    console.log(`${title}: ${date} ${description} ${duration}`);
-  }
   onCancel() {
-    this.location.back();
+    // this.location.back();
+    this.courseForm.reset();
+  }
+
+  onSubmit(): void {
+    console.log(this.courseForm.valid);  // {first: 'Nancy', last: 'Drew'}
   }
 }
---
+++ coursesappp/src/app/validators/custom-validators.ts
@@ -0,0 +1,9 @@
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+
+export function dateFormatValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+        const dateRegExp = new RegExp('^[0-9]{2}[\/]{1}[0-9]{2}[\/]{1}[0-9]{4}$');
+        const isDateFromat = dateRegExp.test(control.value);
+        return isDateFromat ? null : { 'Invalid Format Please Use dd/MM/yyyy': { value: control.value } };
+    };
+}
